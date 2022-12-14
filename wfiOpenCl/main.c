1e67227eb3ae9fae1cb1e7ef7d9efcea8b69610a
Im-Rises <quentin-morel88@hotmail.com>
2022-12-13 19:57:47 -0500

    wip

--- wfiOpenCl/main.c
+++ wfiOpenCl/main.c
@@ -8,6 +8,8 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
+#include "../common/commonFunctions.h"
+
 #define PRINT_MATRIX_THRESHOLD 30
 
 char* readProgramFile(const char* filename) {
@@ -29,71 +31,6 @@ char* readProgramFile(const char* filename) {
     fread(source, sz, 1, fp);
     source[sz] = '\0';
     return source;
-}
-
-int* generateTestMatrix(int n) {
-    int* matrix = (int*)malloc(n * n * sizeof(int));
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (i == j)
-                matrix[i * n + j] = 0;
-            else if (i == j - 1)
-                matrix[i * n + j] = 1;
-            else
-                matrix[i * n + j] = n + 1;
-        }
-    }
-    matrix[(n - 1) * n] = 1;
-    return matrix;
-}
-
-int countDigit(long long n) {
-    if (n / 10 == 0)
-        return 1;
-    return 1 + countDigit(n / 10);
-}
-
-void printMatrix(int* matrix, int n) {
-    if (n > PRINT_MATRIX_THRESHOLD)
-    {
-        printf("Matrix is too large to printed completely.\n");
-        printf("- First row: ");
-        for (int i = 0; i < n; i++)
-        {
-            if (matrix[i] == n + 1)
-                printf("%*s ", countDigit(n + 1), "INF");
-            else
-                printf("%*d ", countDigit(n + 1), matrix[i]);
-        }
-        printf("\n");
-
-        printf("- Last row: ");
-        for (int i = (n - 1) * n; i < n * n; i++)
-        {
-            if (matrix[i] == n + 1)
-                printf("%*s ", countDigit(n + 1), "INF");
-            else
-                printf("%*d ", countDigit(n + 1), matrix[i]);
-        }
-        printf("\n");
-    }
-    else
-    {
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (matrix[i * n + j] == n + 1)
-                    printf("%*s ", countDigit(n + 1), "INF");
-                else
-                    printf("%*d ", countDigit(n + 1), matrix[i * n + j]);
-            }
-            printf("\n");
-        }
-    }
-    printf("\n");
 }
 
 int main(int argc, char* argv[]) {
